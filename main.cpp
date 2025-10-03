451c1e2cb61eaa8988121db804f2a6071b4ec971
theandelope <theandelope16@gmail.com>
2025-10-03 01:13:56 -0400

    feat: element-wise operations

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include "matrix.h"
-
+#include <iostream>
 int main() {
-    Matrix<int> mat(2, 3);
+    Matrix<float> mat(2, 3);
+    Matrix<float> mat1(2, 2);
+    Matrix<float> result(2, 3);
+    result = mat + mat1;
 
+    for(int i = 0; i < mat.shape()[0];i++) {
+      for(int j = 0; j < mat.shape()[1];j++) {
+        std::cout<< result.at(i,j) <<std::endl;
+      }    
+    }
     return 0;
 }
--- matrix.h
+++ matrix.h
@@ -1,6 +1,7 @@
 #ifndef MATRIX_H
 #define MATRIX_H
-
+#include <vector>
+#include <stdexcept>
 template<typename T>
 class Matrix {
     std::vector<T> data;
@@ -23,6 +24,137 @@ public:
         }
         return transposed;
     }
+
+    Matrix<T> operator+(const Matrix<T>& mat2) const {
+
+      if(this->shape()!=mat2.shape()) throw std::invalid_argument("Matrix Shapes Must Match");
+
+      Matrix<T> resultant(this->shape()[0],this->shape()[1]);
+      for(int i=0;i<this->shape()[0];i++) {
+        for(int j=0;j<this->shape()[1];j++) {
+         resultant.at(i, j) = this->at(i, j) +mat2.at(i, j);
+        }
+      }
+      return resultant;
+    }
+
+    Matrix<T> operator+(const T num) const {
+      Matrix<T> resultant(this->shape()[0],this->shape()[1]);
+      for(int i=0;i<this->shape()[0];i++) {
+        for(int j=0;j<this->shape()[1];j++) {
+         resultant.at(i, j) = this->at(i, j) + num;
+        }
+      }
+      return resultant;
+    }
+
+    Matrix<T> operator-(const Matrix<T>& mat2) const {
+
+      if(this->shape()!=mat2.shape()) throw std::invalid_argument("Matrix Shapes Must Match");
+
+      Matrix<T> resultant(this->shape()[0],this->shape()[1]);
+      for(int i=0;i<this->shape()[0];i++) {
+        for(int j=0;j<this->shape()[1];j++) {
+         resultant.at(i, j) = this->at(i, j) -imat2.at(i, j);
+        }
+      }
+      return resultant;
+    }
+
+    Matrix<T> operator-(const T num) const {
+      Matrix<T> resultant(this->shape()[0],this->shape()[1]);
+      for(int i=0;i<this->shape()[0];i++) {
+        for(int j=0;j<this->shape()[1];j++) {
+         resultant.at(i, j) = this->at(i, j) - num;
+        }
+      }
+      return resultant;
+    }    Matrix<T> operator*(const Matrix<T>& mat2) const {
+
+      if(this->shape()!=mat2.shape()) throw std::invalid_argument("Matrix Shapes Must Match");
+
+      Matrix<T> resultant(this->shape()[0],this->shape()[1]);
+      for(int i=0;i<this->shape()[0];i++) {
+        for(int j=0;j<this->shape()[1];j++) {
+         resultant.at(i, j) = this->at(i, j) *mat2.at(i, j);
+        }
+      }
+      return resultant;
+    }
+
+    Matrix<T> operator*(const T num) const {
+      Matrix<T> resultant(this->shape()[0],this->shape()[1]);
+      for(int i=0;i<this->shape()[0];i++) {
+        for(int j=0;j<this->shape()[1];j++) {
+         resultant.at(i, j) = this->at(i, j) / num;
+        }
+      }
+      return resultant;
+    }    Matrix<T> operator/(const Matrix<T>& mat2) const {
+
+      if(this->shape()!=mat2.shape()) throw std::invalid_argument("Matrix Shapes Must Match");
+
+      Matrix<T> resultant(this->shape()[0],this->shape()[1]);
+      for(int i=0;i<this->shape()[0];i++) {
+        for(int j=0;j<this->shape()[1];j++) {
+         if(mat2.at(i, j)==0) throw std::invalid_argument("Division by 0");
+
+         resultant.at(i, j) = this->at(i, j) +mat2.at(i, j);
+        }
+      }
+      return resultant;
+    }
+
+    Matrix<T> operator/(const T num) const {
+    if(num==0) throw std::invalid_argument("Division by 0");
+
+      Matrix<T> resultant(this->shape()[0],this->shape()[1]);
+
+      for(int i=0;i<this->shape()[0];i++) {
+        for(int j=0;j<this->shape()[1];j++) {
+         resultant.at(i, j) = this->at(i, j) + num;
+        }
+      }
+      return resultant;
+    }
+
+/*
+
+    operator-(Matrix<T> mat1, Matrix<T> mat2) {
+      for(int i;i<mat1.shape[0];i++) {
+        int 
+      }
+
+    }
+
+    operator*(Matrix<T> mat1, Matrix<T> mat2) {
+      for(int i;i<mat1.shape[0];i++) {
+        int 
+      }
+
+    }
+
+    operator/(Matrix<T> mat1, Matrix<T> mat2) {
+      for(int i;i<mat1.shape[0];i++) {
+        int 
+      }
+
+    }
+
+    operator^(Matrix<T> mat1, Matrix<T> mat2) {
+      for(int i;i<mat1.shape[0];i++) {
+        int 
+      }
+    
+    }
+
+    Matrix<T> ln(Matrix<T> mat) {
+      for(int i;i<mat1.shape[0];i++) {
+        int 
+      }
+
+    }
+*/    
 };
 
 #endif
